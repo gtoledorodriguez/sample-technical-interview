ff9eb78cbc4b7ccd76876f5834497c1bdf2c6407
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-01-24 20:06:16 +0000

    fixed errors

--- birthday_cake.rb
+++ birthday_cake.rb
@@ -29,6 +29,13 @@ class BirthdayCake
     self.lit = false
   end
 
+  def light_candles
+      self.lit = true
+  end
+
+  def blow_out_candles
+    self.lit = false
+  end
   def candles_status
     if self.lit == true
       return "lit"
@@ -50,20 +57,19 @@ class BirthdayCake
   end
 
   def BirthdayCake.celebrate(age)
-    birthday_cake = Birthdaycake.new(age)
+    birthday_cake = BirthdayCake.new(age)
     puts birthday_cake.greet
     puts birthday_cake.to_s
     puts "Lighting candles..."
     birthday_cake.light_candles
     puts birthday_cake.to_s
     puts "Singing happy birthday..."
-    puts birthday_cake.sign
+    puts birthday_cake.sing
     puts "Blowing out candles..."
-    birth_cake.blow_out_candles
+    birthday_cake.blow_out_candles
     puts birthday_cake.to_s
 
     birthday_cake
   end
 end
-
 BirthdayCake.celebrate(10)
